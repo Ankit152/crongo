091de9dcf1bcb1009b61560be6948bd7afcc5ebc
Ankit152 <ankitkurmi152@gmail.com>
2022-03-25 14:04:10 +0530

    added multi versions of api and fixed setup of controller manager

--- api/v2/cronjob_types.go
+++ api/v2/cronjob_types.go
@@ -19,12 +19,12 @@ package v2
 import (
 	batchv1beta1 "k8s.io/api/batch/v1beta1"
 	corev1 "k8s.io/api/core/v1"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // CronJobSpec defines the desired state of CronJob
 type CronJobSpec struct {
 	// The schedule in Cron format, see https://en.wikipedia.org/wiki/Cron.
